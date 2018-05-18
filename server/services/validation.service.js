eb25fa0c36a69a1c2213a8d4898e2a25adad8e8f
Jhymer Martínez <jhmartinez1991@gmail.com>
2018-05-18 11:17:37 -0500

    Add 'background' in the Campaign model

--- server/services/validation.service.js
+++ server/services/validation.service.js
@@ -2,3 +2,28 @@ export const isValidEmail = (value) => {
   const emailPattern = /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i;  // eslint-disable-line
   return emailPattern.test(value);
 }
+
+export const isValidRgbColor = (value) => {
+  const rgbExp = /rgb\(\d{1,3},\s?\d{1,3},\s?\d{1,3}\)/; // eslint-disable-line
+  return rgbExp.test(value);
+}
+
+export const isValidHexColor = (value) => {
+  const hexExp = /^#\w{6,8}/; // eslint-disable-line
+  return hexExp.test(value);
+}
+
+export const isValidUrl = (value) => {
+  const urlExp = /^http/; // eslint-disable-line
+  return urlExp.test(value);
+}
+
+export const isValidLinearGradient = (value) => {
+  const linearExp = /^linear-gradient/; // eslint-disable-line
+  return linearExp.test(value);
+}
+
+export const isValidRadialGradient = (value) => {
+  const RadialExp = /^radial-gradient/;; // eslint-disable-line
+  return RadialExp.test(value);
+}
